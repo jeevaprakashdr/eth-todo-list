0c3147ef47b181fbd219b4f8732d0c72e457b3c1
jeeva <jeeva.ramu@codurance.com>
2022-11-25 10:57:41 +0000

    added the client index html page

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    constructor() public {
+        createtask("learn truffle");
+        createtask("go to the website, https://trufflesuite.com/");
+    }
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createtask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
